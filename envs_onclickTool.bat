07bf11a01d6e72f8b0c3d1cc6e6aa3371c6e2801
h3136514 <3136514@naver.com>
2023-12-03 14:51:00 +0900

    upload project

---
+++ envs_onclickTool.bat
@@ -0,0 +1,10 @@
+rem 윈도우에서 작동하는 환경변수 bat파일. cmd창에서 실행하면 됨
+
+set OAUTHLIB_INSECURE_TRANSPORT=1
+
+set SECRET_KEY=dwdGTJaTOFI1RFqnuomyTssa11313
+
+set DB_USER=DB이름
+set DB_PW=1111
+set GOOGLE_CLIENT_ID=11.....-.apps.googleusercontent.com
+set GOOGLE_CLIENT_SECRET=GO.....
